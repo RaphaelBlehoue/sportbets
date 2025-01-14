5d53fbd91e6bf90e2944a7951f34537c901c9245
SAMPAN Blehoue Hermann Raphael <raphaelsampan@gmail.com>
2025-01-15 00:34:36 +0100

    chore: update vite & eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { configs as tsConfigs } from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -12,12 +15,23 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: parser, // Ajout explicite du parseur TypeScript
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    linterOptions: {
+      reportUnusedDisableDirectives: true, // Ajout correct de cette option
+    },
+    settings : {
+      react: {
+        version: 'detect',
+      }
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tsConfigs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,55 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker'
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    }
-  }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      // Configuration du plugin `vite-plugin-checker`
+      typescript: true, // Active la vérification TypeScript
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint --config eslint.config.js "./src/**/*.{js,jsx,ts,tsx}"', // Commande pour vérifier tous les fichiers TypeScript et React
+        dev: {logLevel: ['error']},
+
+      },
+    }),
+    eslintPlugin({
+      formatter: 'stylish',
+      eslintOptions: {
+        cache: true
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  envPrefix: 'VITE_',
+  server: {
+    port: 13370,
+    open: true, // Ouvre automatiquement le navigateur lors du démarrage du serveur
+  },
+  resolve: {
+    alias: {
+      '~assets': '/src/assets', // Alias pour accéder facilement aux ressources dans src/assets
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/main.scss";`, // Import global du fichier SCSS
+      },
+    },
+  },
+  define: {
+    /**
+     * Variable APP_VERSION pour obtenir la version de l'application
+     * Si `sportbet_app_version` n'est pas défini, on utilise `npm_package_version` ou "unknown" en fallback
+     */
+    APP_VERSION: JSON.stringify(process.env.sportbet_app_version ?? process.env.npm_package_version ?? 'unknown'),
+  },
 });
